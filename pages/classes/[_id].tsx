9c899d44cc4e1612b842d592ab3522c5ae302e52
Yossa Michel <ngambmicheal@gmail.com>
2021-12-15 12:28:48 +0100

    update ui

--- pages/classes/[_id].tsx
+++ pages/classes/[_id].tsx
@@ -81,6 +81,7 @@ export default function ClasseDetails(){
 
     const closeDynamicExamModal = () => {
       setDynamicExamIsOpen(s => false);
+      getTerms();
   }
 
     const closeImportModal = () => {
